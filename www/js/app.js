cb82ea1d41cea6d1bd2111cc83da2a8fcbf4a3fb
aphilip97 <33058483+aphilip97@users.noreply.github.com>
2018-04-05 18:01:08 +0100

    Tested on Android
    Android test files

--- www/js/app.js
+++ www/js/app.js
@@ -12,6 +12,7 @@
   ]);
 
   app.run(function ($ionicPlatform, $rootScope, activityevents) {
+    
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins.Keyboard) {
         cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -21,18 +22,19 @@
         StatusBar.styleDefault();
       }
     });
-
+    
     $rootScope.$on('$stateChangeStart', function (e, toState, toParams, fromState, fromParams, options) {
       console.log('$stateChangeStart fired');
       console.log('Moving from ' + fromState.url + ' to ' + toState.url);
     });
-
+    
     $rootScope.$on('$stateChangeSuccess', function (e, toState, toParams, fromState, fromParams) {
       console.log('$stateChangeSuccess fired');
       console.log('Moved to ' + toState.url + ' from ' + fromState.url);
     });
-
+    
     activityevents.configureHTTP_BASICAuthentication('a4ccb4f9-f183-4505-b178-cca604d6c678', '4e39077a-383c-407c-a538-dd2c7cda3dfc');
+    
 
   });
 
